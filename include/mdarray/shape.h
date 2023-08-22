690b9e9e997ed5e5d7c66eb96f7151aacd9c9811
hastws <hastws@hotmail.com>
2023-08-23 01:31:00 +0800

    refactor: move shape definitions to header

--- include/mdarray/shape.h
+++ include/mdarray/shape.h
@@ -13,13 +13,17 @@ namespace KD {
 class Shape {
  public:
   // constructor
-  Shape(std::initializer_list<Index> dims);
+  Shape(std::initializer_list<Index> dims) : dims_(dims) {}
 
-  Shape(const Shape &other, Index skip);
+  Shape(const Shape &other, Index skip) : dims_(other.DimensionsSize() - 1) {
+    Index i = 0;
+    for (; i < skip; ++i) dims_[i] = other.dims_[i];
+    for (; i < dims_.ArraySize(); ++i) dims_[i] = other.dims_[i + 1];
+  }
 
-  Shape(Index *dims, Index dim);
+  Shape(Index *dims, Index dim_) : dims_(dims, dim_) {}
 
-  Shape(IndexArray &&shape);
+  Shape(IndexArray &&shape) : dims_(std::move(shape)) {}
 
   Shape(const Shape &other) = default;
 
@@ -28,13 +32,29 @@ class Shape {
   ~Shape() = default;
 
   // method
-  Index SpaceSize() const;
+  Index SpaceSize() const {
+    Index res = 1;
+    for (Index i = 0; i < dims_.ArraySize(); ++i) res *= dims_[i];
+    return res;
+  }
 
-  Index SubSpaceSize(Index start_dim, Index end_dim) const;
+  Index SubSpaceSize(Index start_dim, Index end_dim) const {
+    Index res = 1;
+    for (; start_dim < end_dim; ++start_dim) res *= dims_[start_dim];
+    return res;
+  }
 
-  Index SubSpaceSize(Index start_dim) const;
+  Index SubSpaceSize(Index start_dim) const {
+    return SubSpaceSize(start_dim, dims_.ArraySize());
+  }
 
-  bool operator==(const Shape &other) const;
+  bool operator==(const Shape &other) const {
+    if (this->DimensionsSize() != other.DimensionsSize()) return false;
+    Index i = 0;
+    for (; i < dims_.ArraySize() && dims_[i] == other.dims_[i]; ++i)
+      ;
+    return i == dims_.ArraySize();
+  }
 
   // inline function
   Index DimensionsSize() const { return dims_.ArraySize(); }
